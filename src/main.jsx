78c3c5005537f65f3287aafd25b9a48fff8df7f9
genaiwithme-star <genaiwithme@gmail.com>
2025-10-04 11:37:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
+import './index.css';   // ✅ Make sure this line exists to load Tailwind CSS
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
