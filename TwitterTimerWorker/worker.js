d5b0416a1a3cf4d5d0a53fb8e89e4c8e9e9a8467
genaiwithme-star <genaiwithme@gmail.com>
2025-10-05 19:37:25 +0530

    Add files via upload

---
+++ TwitterTimerWorker/worker.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+module.exports = async function (context, myTimer) {
+    const timeStamp = new Date().toISOString();
+    context.log('Twitter worker triggered at', timeStamp);
+
+    const bearerToken = process.env.TWITTER_BEARER_TOKEN;
+
+    if (!bearerToken) {
+        context.log('TWITTER_BEARER_TOKEN not set in Application Settings!');
+        return;
+    }
+
+    try {
+        const response = await axios.get('https://api.twitter.com/2/tweets/sample/stream', {
+            headers: {
+                Authorization: `Bearer ${bearerToken}`
+            }
+        });
+
+        context.log('Twitter API response received:', response.status);
+
+    } catch (error) {
+        context.log('Error fetching Twitter API:', error.message);
+    }
+};
